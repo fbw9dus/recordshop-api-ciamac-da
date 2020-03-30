e51fd7dc5dfedda347cf427a2096b302faf5d37e
ciamac <hgzguzg>
2020-03-30 12:25:55 +0200

    Verified again an again

--- middleware/security.js
+++ middleware/security.js
@@ -1,10 +1,17 @@
-exports.setCors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, x-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+const { validationResult } = require('express-validator')
 
-  next();
-};
+const validateInputs = rules => {
+    return [
+        ...rules,
+        (req, res, next) => {
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(422).json({errors: errors.array()})
+            }
+            
+            next()
+        }
+    ]
+}
+
+module.exports = {validateInputs}
