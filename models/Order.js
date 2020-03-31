4e65982c59d50a4d650ffd2fb43002985cadcf26
ciamac <hgzguzg>
2020-03-31 12:48:31 +0200

    task7 the second one!

--- models/Order.js
+++ models/Order.js
@@ -24,7 +24,7 @@ const RecordSchema = new Schema({
   }
   
 });
-const Record = mongoose.model("Record", RecordSchema)
+const Record = mongoose.model('Record', RecordSchema)
 
 
 const OrderSchema = new Schema({
@@ -46,4 +46,4 @@ const Order = mongoose.model('Order', OrderSchema)
 const recordorder =  Order.findById().populate('records')
 console.log(recordorder)
 
-module.exports = mongoose.model("recordorder", OrderSchema);
+module.exports = mongoose.model("Order", OrderSchema);
