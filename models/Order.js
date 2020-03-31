df8885723b3501a4c45cb63c0042e47cc28fbc1a
ciamac <hgzguzg>
2020-03-31 12:36:07 +0200

    task7 the second one

--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,32 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const RecordSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  img: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+  
+});
+const Record = mongoose.model(Record, RecordSchema)
+
+
 const OrderSchema = new Schema({
   quantity: {
     type: Number,
@@ -9,7 +35,15 @@ const OrderSchema = new Schema({
   record: {
     type: Number,
     required: true
+  },
+  records:{
+    ref:"Record",
+    type: mongoose.Schema.Types.ObjectId,
   }
 });
+const Order = mongoose.model('Order', OrderSchema)
 
-module.exports = mongoose.model("Order", OrderSchema);
+const recordorder =  Order.findById().populate('records')
+console.log(recordorder)
+
+module.exports = mongoose.model("recordorder", OrderSchema);
