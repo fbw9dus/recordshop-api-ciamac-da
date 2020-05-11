234abf8c111e74ed3ff59f7a31e28b6a27316fa9
ciamac <hgzguzg>
2020-05-11 13:56:02 +0200

    Vrified

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const multer = require("multer")
+const upload = multer({ dest: 'date/'})
+ul
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+
+/**POST register form */
+router.post("/register" , upload.single('avatar'), (req,res)=>{
+  console.log(req.file)
+  res.json({message: `received ${req.file}`})
+})
 module.exports = router;
